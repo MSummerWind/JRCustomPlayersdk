e30cd6ff562e76f6c1c5a86f7c67ce2b9ec97813
苗向前 <miaoxiangqian@jinred.com>
2022-05-24 18:32:17 +0800

    upload framework

--- Example/Pods/Target Support Files/JRCustomPlayerSdk/JRCustomPlayerSdk-umbrella.h
+++ Example/Pods/Target Support Files/JRCustomPlayerSdk/JRCustomPlayerSdk-umbrella.h
@@ -10,6 +10,32 @@
 #endif
 #endif
 
+#import "AliMediaDownloader.h"
+#import "AliyunMediaDownloader.h"
+#import "AVDConfig.h"
+#import "AliListPlayer.h"
+#import "AliMediaLoader.h"
+#import "AliPlayer.h"
+#import "AliPlayerGlobalSettings.h"
+#import "AliPrivateService.h"
+#import "AliVodMediaLoader.h"
+#import "AliyunPlayer.h"
+#import "AMDDelegate.h"
+#import "AVPCacheConfig.h"
+#import "AVPConfig.h"
+#import "AVPDef.h"
+#import "AVPDelegate.h"
+#import "AVPErrorCode.h"
+#import "AVPFilterConfig.h"
+#import "AVPLiveKeyGenerator.h"
+#import "AVPLiveTimeShift.h"
+#import "AVPMediaInfo.h"
+#import "AVPSource.h"
+#import "CicadaAudioSessionDelegate.h"
+#import "CicadaFrameInfo.h"
+#import "CicadaRenderDelegate.h"
+#import "CicadaRenderingDelegate.h"
+#import "VidPlayerConfigGen.h"
 
 FOUNDATION_EXPORT double JRCustomPlayerSdkVersionNumber;
 FOUNDATION_EXPORT const unsigned char JRCustomPlayerSdkVersionString[];
